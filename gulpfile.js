a9e547e79f70dbdd80cb716654fc18bbfa1b5d81
Masayoshi Wada <developer@andantesoftware.com>
2015-09-14 17:19:54 +0900

    browserSyncをいれた

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var minifyCss = require('gulp-minify-css');
+var browserSync = require('browser-sync');
 
 var LESS_ROOT = './src/less';
 var CSS_ROOT = './dist/css';
+var EXAMPLE_ROOT = './example';
 
 gulp.task('less', function() {
   return gulp.src(LESS_ROOT + '/app.less')
@@ -14,6 +16,18 @@ gulp.task('less', function() {
 
 gulp.task('watch', function() {
   gulp.watch(LESS_ROOT + '/**', ['less']);
+});
+
+gulp.task('server', function() {
+  browserSync({
+    server: {
+      baseDir: EXAMPLE_ROOT
+    }
+  });
+
+  gulp.watch(LESS_ROOT + '/**', ['less', function() {
+    browserSync.reload();
+  }]);
 });
 
 gulp.task('default', ['less'], function() {});
